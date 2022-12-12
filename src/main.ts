eeae27a68771b294babc12af0ba2792c2d107324
DmitriyNoa <dmytro.zharkov@gmail.com>
2022-12-12 12:54:21 +0100

    Listen log

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
   app.setGlobalPrefix('api');
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
+  console.log(`-----app is listening on port ${port}-----`);
 }
 bootstrap();
